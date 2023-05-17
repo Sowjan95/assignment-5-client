84812187dff394466d00b53cd187800ed8ee8f4d
Sowjan95 <sowjanya.sritharasarma@macaulay.cuny.edu>
2023-05-16 22:53:36 -0400

    added delete button to single student view

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -4,14 +4,25 @@ StudentView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the single student view page.
 ================================================== */
+import { Link, useHistory } from "react-router-dom";
+
+// Take in props data to construct the component
 const StudentView = (props) => {
   const { student } = props;
+  const { deleteStudent } = props;
+  let history = useHistory();
+
+  function deleteAndRedirect() {
+    deleteStudent(student.id);
+    history.push("/students");
+  }
 
   // Render a single Student view 
   return (
     <div>
       <h1>{student.firstname + " " + student.lastname}</h1>
       <h3>{student.campus.name}</h3>
+      <button onClick={deleteAndRedirect}>Delete</button>
     </div>
   );
 
